c30b81d01b15004ebce63d2910a0ec4d8d7e118a
Асылжан Баймуратов <asylzanbajmuratov@Noutbuk-Asylzan.local>
2024-06-10 23:03:16 +0500

    lesson-0030

---
+++ src/FunctionOrMethod.java
@@ -0,0 +1,13 @@
+public class FunctionOrMethod {
+    int value;
+    void changeValue(){
+        value = 65;
+    }
+
+    public static void main(String[] args) {
+        FunctionOrMethod obj = new FunctionOrMethod();
+        System.out.println(obj.value);
+        obj.changeValue();
+        System.out.println(obj.value);
+    }
+}
