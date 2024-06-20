3c738ee252848e73ea87bfc4b700926f65ce9009
Асылжан Баймуратов <asylzanbajmuratov@Noutbuk-Asylzan.local>
2024-06-20 15:08:33 +0500

    lesson-0032

---
+++ src/GC.java
@@ -0,0 +1,9 @@
+public class GC {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            int[] array = new int[100];
+        }
+
+        int[] array = new int[100];
+    }
+}
