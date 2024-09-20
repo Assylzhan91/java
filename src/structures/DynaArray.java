005344ee2a87797f44cc90910dfc14ac352391a3
Асылжан Баймуратов <asylzanbajmuratov@Noutbuk-Asylzan.local>
2024-09-20 21:10:00 +0500

    lesson-0085

--- src/structures/DynaArray.java
+++ src/structures/DynaArray.java
@@ -1,34 +1,31 @@
 package structures;
 
-import somepackage.ThisUsage;
-
 import java.util.Arrays;
 
 public class DynaArray {
     private int[] result;
     private int count;
 
-    public DynaArray(){
+    public DynaArray() {
         result = new int[5];
-        ThisUsage thisUsage1 = new ThisUsage();
-        ThisUsage thisUsage2 = new ThisUsage(10);
     }
 
-    public DynaArray(int size){
+    public DynaArray(int size) {
         result = new int[size];
     }
 
-    public void add( int  value) {
+    public void add(int value) {
         if (count == result.length) {
-            grow(result.length == 0 ?  5 : result.length * 2);
+            grow(result.length == 0 ? 5 : result.length * 2);
         }
         result[count++] = value;
     }
 
-    public void add( int[] array) {
+    public void add(int[] array) {
         add(array, array.length);
     }
-    public void add( DynaArray dynaArray) {
+
+    public void add(DynaArray dynaArray) {
         add(dynaArray.result, dynaArray.count);
     }
 
@@ -40,25 +37,56 @@ public class DynaArray {
         count += length;
     }
 
-    private void grow(int newLength){
+    private void grow(int newLength) {
         int[] newArray = new int[newLength];
         System.arraycopy(result, 0, newArray, 0, result.length);
         result = newArray;
     }
 
-   public int[] toArray() {
-       return Arrays.copyOf(result, count);
+    public int[] toArray() {
+        return Arrays.copyOf(result, count);
     }
 
-    public String  asString() {
+    public void clear() {
+        count = 0;
+    }
+
+    public String asString() {
         final StringBuilder stringBuilder = new StringBuilder().append('[');
         for (int i = 0; i < count; i++) {
             stringBuilder.append(result[i]);
-            if (i <  count - 1) {
+            if (i < count - 1) {
                 stringBuilder.append(',').append(' ');
             }
         }
 
         return stringBuilder.append(']').toString();
-     }
+    }
+
+    public boolean remove(int value) {
+        int index = findIndex(value);
+        if (index != -1 ) {
+            removeByIndex(index);
+            return true;
+        }
+        return false;
+    }
+
+    private void removeByIndex(int index) {
+        if (index < count - 1) {
+            System.arraycopy(result, index + 1, result, index, count - index - 1);
+        }
+
+        count--;
+    }
+
+    private int findIndex(int value){
+        for (int i = 0; i < count; i++) {
+            if (result[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
