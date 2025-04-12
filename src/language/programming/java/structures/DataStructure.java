b7fd46a059fa5cfde63853cfb9936a825adf5dac
Асылжан Баймуратов <asylzanbajmuratov@Noutbuk-Asylzan.local>
2025-04-12 21:53:44 +0500

    module-11-013

--- src/language/programming/java/structures/DataStructure.java
+++ src/language/programming/java/structures/DataStructure.java
@@ -23,24 +23,24 @@ package language.programming.java.structures;
 /**
  * @Assylzhan Baimuratov
  **/
-public abstract class DataStructure {
+public interface DataStructure {
 
-    public abstract void add(int value);
+     void add(int value);
 
-    public abstract void add(int[] array);
+     void add(int[] array);
 
-    public abstract void add(DynaArray dynaArray);
+     void add(DynaArray dynaArray);
 
-    public abstract void add(LinkedList list);
+     void add(LinkedList list);
 
-    public abstract int size();
+     int size();
 
-    public abstract void clear();
+     void clear();
 
-    public abstract int[] toArray();
+     int[] toArray();
 
-    public abstract boolean remove(int value);
+     boolean remove(int value);
 
-    public abstract boolean contains(int value);
+     boolean contains(int value);
 
 }
